6c1d119467aa202424d40b6176b62a10ca4bebde
adrian-burgos-tome <aburtom0312@g.educaand.es>
2025-10-23 12:20:42 +0200

    practica 2

--- practica2/src/Main.java
+++ practica2/src/Main.java
@@ -1,9 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello world 2");
+import java.util.Scanner;
+void main() {
+    Scanner scanner = new Scanner(System.in);
+    int i;
+    do {
+        System.out.print("Introduzca el número de lados de un polígono (3-4)");
+        i = scanner.nextInt();
+        if (i < 3 || i > 4) {
+            System.out.println("Error: El número debe estar entre 3 y 10. Inténtelo de nuevo.");
+        }
+    } while (i < 3 || i > 10); // La condición del bucle: se repite si 'i' no está en el rango
+    System.out.println("Número válido introducido: " + i);
+    System.out.println("Introduzca la altura del poligono");
+    int a = scanner.nextInt();
+    System.out.println("Introduzca la base del poligono");
+    int b = scanner.nextInt();
+    if (i==3){
+        int c=(b*a)/2;
+        System.out.println("El área es= " + c );
+    }
+    if (i==4){
+        int d =b*a;
+        System.out.println("El área es= " + d);
     }
 }
